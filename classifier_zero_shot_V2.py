50b7bce17520578797803255726371fec925bd20
Aveklan <avesani55@gmail.com>
2024-12-18 23:29:42 +0100

    Created new dataset combining examples with hate speech, hate speech against people with disabilities and non hate speech. Updated the classification file to compe with this new dataset.

---
+++ classifier_zero_shot_V2.py
@@ -0,0 +1,71 @@
+import json
+from ollama import chat, ChatResponse
+from pathlib import Path
+from src.scripts.gab_hate import get_gab_columns
+
+root_dir = Path(__file__).resolve().parent.parent
+
+
+def get_dataset():
+    json_file_path = root_dir / "Project" / "src" / "dataset" / "cad_dataset.json"
+    with open(json_file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data
+
+
+# Function to classify a comment
+def classify_comment(comment, model):
+    prompt = f"The following comment needs to be classified. Does it contain hate speech against people with disabilities? Respond only with 'yes' or 'no'. Do not provide any explanations or generate other text.\nComment: {comment}"
+    response: ChatResponse = chat(
+        model=model,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+    )
+    # Extract and print the classification result
+    return response["message"]["content"]
+
+
+def classify_dataset(data, model, entry_name):
+    output_file_path = (
+        root_dir / "Project" / "src" / "dataset" / "cad_dataset_classified.json"
+    )
+    processed_comments = 0
+    total_comments = len(data)
+
+    for entry in data:
+        comment = entry.get("comment", "")
+        result = classify_comment(comment, model)
+        entry[entry_name] = result  # Add the result to the entry
+
+        processed_comments += 1
+        progress = (processed_comments / total_comments) * 100
+        print(f"Progress: {progress:.2f}% ({processed_comments}/{total_comments})")
+
+        if processed_comments % 100 == 0:
+            with open(output_file_path, "w", encoding="utf-8") as output_file:
+                json.dump(data, output_file, indent=4)
+            print(f"Progress saved at {processed_comments} comments.")
+
+    # Final save to ensure all data is written
+    with open(output_file_path, "w", encoding="utf-8") as output_file:
+        json.dump(data, output_file, indent=4)
+
+    print(
+        f"{model} Classification completed, resut can be found in JSON file {output_file_path}"
+    )
+
+
+if __name__ == "__main__":
+    data = get_dataset()
+    print("Starting classification with llama model...")
+    classify_dataset(data, "llama3.1:8b", "result_llama")
+
+    print("Starting classification with gemma model...")
+    classify_dataset(data, "gemma", "result_gemma")
+
+    print("Starting classification with mistral model...")
+    classify_dataset(data, "mistral", "result_mistral")
---
+++ src/dataset/combine_full_datasets.py
@@ -0,0 +1,78 @@
+from math import nan
+import pandas as pd
+from pathlib import Path
+import pandas as pd
+import json
+
+root_dir = Path(__file__).resolve().parent.parent
+
+
+def cad():
+    print("Creating CAD dataset...")
+    file_path = root_dir / "dataset" / "cad_v1.tsv"
+    dfCAD = pd.read_csv(file_path, delimiter="\t")
+    dfCAD_filtered = dfCAD[
+        [
+            "meta_text",
+            "annotation_Primary",
+            "annotation_Secondary",
+            "annotation_Target_top.level.category",
+        ]
+    ]
+    dfCAD_filtered = dfCAD_filtered.rename(
+        columns={"annotation_Target_top.level.category": "annotation_category"}
+    )
+    dfCAD_filtered = dfCAD_filtered[dfCAD_filtered["meta_text"].notna()]
+
+    dfCAD_filtered_identityDirected = dfCAD_filtered[
+        dfCAD_filtered["annotation_Primary"] == "IdentityDirectedAbuse"
+    ]
+    n = len(dfCAD_filtered_identityDirected)
+
+    dfCAD_filtered_personDirectedAbuse = dfCAD_filtered[
+        dfCAD_filtered["annotation_Primary"] == "PersonDirectedAbuse"
+    ].head(n)
+    dfCAD_filtered_nonHate = dfCAD_filtered[
+        dfCAD_filtered["annotation_Primary"] == "Neutral"
+    ].head(n)
+
+    cad_dataset = pd.concat(
+        [
+            dfCAD_filtered_identityDirected,
+            dfCAD_filtered_personDirectedAbuse,
+            dfCAD_filtered_nonHate,
+        ]
+    )
+
+    return cad_dataset
+
+
+def create_dataset(dataset):
+    filtered_data = []
+    json_file_path = root_dir / "dataset" / "cad_dataset.json"
+
+    for _, row in dataset.iterrows():
+        # Create the JSON structure for each row
+        entry = {
+            "comment": row["meta_text"],
+            "annotation_Primary": row["annotation_Primary"],
+            "annotation_category": row["annotation_category"],
+        }
+        # Append the entry to the list
+        filtered_data.append(entry)
+
+    # Save the list of structured data to a JSON file
+    with open(json_file_path, "w", encoding="utf-8") as file:
+        json.dump(filtered_data, file, indent=4, ensure_ascii=False)
+
+    print(f"Data successfully saved to {json_file_path}")
+
+
+def main():
+    print("Creating Datasets...")
+    identityDirected = cad()
+    create_dataset(identityDirected)
+
+
+if __name__ == "__main__":
+    main()
