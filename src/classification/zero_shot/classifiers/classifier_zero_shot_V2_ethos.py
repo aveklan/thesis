aad64d2810ddd2340435ab4a51e8f1c1ca543645
aveklan <avesani55@gmail.com>
2025-01-18 13:43:24 +0100

    Fixed wrong output path

--- src/classification/zero_shot/classifiers/classifier_zero_shot_V2_ethos.py
+++ src/classification/zero_shot/classifiers/classifier_zero_shot_V2_ethos.py
@@ -36,7 +36,7 @@ def classify_comment(comment, model):
 def classify_dataset(data, model, entry_name):
     output_file_path = (
         root_dir
-        / "results"
+        / "classification"
         / "few_shot"
         / "classification_results"
         / "ethos_dataset_withContext_classified_zero_shot_second_attempt.json"
